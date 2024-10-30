264937ffbe4308b8d0d1f3240d119d6fbfea4f41
Dima Artamonov <artamonovdima355@gmail.com>
2024-10-30 18:03:38 +0200

    Add unit tests for ProductService

--- src/main/java/com/cosmo/cats/api/service/impl/ProductServiceImpl.java
+++ src/main/java/com/cosmo/cats/api/service/impl/ProductServiceImpl.java
@@ -1,103 +1,80 @@
 package com.cosmo.cats.api.service.impl;
 
+import com.cosmo.cats.api.data.ProductRepository;
 import com.cosmo.cats.api.domain.category.Category;
 import com.cosmo.cats.api.domain.product.Product;
 import com.cosmo.cats.api.service.ProductService;
 import com.cosmo.cats.api.service.exception.DuplicateProductNameException;
 import com.cosmo.cats.api.service.exception.ProductNotFoundException;
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
-  private final List<Product> products = new ArrayList<>(buildAllProductsMock());
 
-  @Override
-  public List<Product> getProducts() {
-    return products;
-  }
+    private final ProductRepository productRepository;
 
-  @Override
-  public Product getProduct(Long id) {
-    return products.stream()
-        .filter(product -> product.getId().equals(id))
-        .findFirst()
-        .orElseThrow(() -> new ProductNotFoundException(id));
-  }
-
-  @Override
-  public Product createProduct(Product product, Long categoryId) {
-    // implement category check logic later with database
-    if (existByName(product.getName())) {
-      throw new DuplicateProductNameException(product.getName());
+    @Override
+    public List<Product> getProducts() {
+        return productRepository.getAll();
     }
-     Product newProduct = product.toBuilder()
-        .category(Category.builder().id(categoryId).name("Space item").build())
-        .id((long) (products.size() + 1))
-        .build();
-    products.add(newProduct);
-    return newProduct;
-  }
 
-  @Override
-  public Product updateProduct(Long id, Product updatedProduct, Long categoryId) {
-    if (!existById(id)) {
-      return createProduct(updatedProduct, categoryId);
+    @Override
+    public Product getProduct(Long id) {
+        return productRepository.getById(id).orElseThrow(() -> new ProductNotFoundException(id));
     }
-    Product existingProduct = getProduct(id);
-    if (existByName(updatedProduct.getName())
-        && !updatedProduct.getName().equals(existingProduct.getName())) {
-      throw new DuplicateProductNameException(updatedProduct.getName());
+
+    @Override
+    public Product createProduct(Product product, Long categoryId) {
+        Product newProduct = buildProduct(product, categoryId, setId());
+        return productRepository.addProduct(newProduct);
     }
-    products.remove(existingProduct);
-    Product productWithUpdates = updatedProduct.toBuilder()
-        .category(Category.builder().id(categoryId).name("Space item").build())
-        .id(id).build();
-    products.add(productWithUpdates);
-    return productWithUpdates;
-  }
 
-  @Override
-  public void deleteProduct(Long id) {
-    Product productToDelete = getProduct(id);
-    products.remove(productToDelete);
-  }
-  private boolean existByName(String productName){
-    return products.stream().anyMatch(product -> product.getName().equals(productName));
-  }
+    @Override
+    public Product updateProduct(Long id, Product updatedProduct, Long categoryId) {
+        if (!existById(id)) {
+            return productRepository.addProduct(buildProduct(updatedProduct, categoryId, id));
+        }
+        Product existingProduct = getProduct(id);
+        if (existByName(updatedProduct.getName())
+                && !updatedProduct.getName().equals(existingProduct.getName())) {
+            throw new DuplicateProductNameException(updatedProduct.getName());
+        }
+        Product productWithUpdates = buildProduct(updatedProduct, categoryId, id);
+        productRepository.update(id, updatedProduct);
+        return productWithUpdates;
+    }
 
-  private boolean existById(Long productId){
-    return products.stream().anyMatch(product -> product.getId().equals(productId));
-  }
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.delete(id);
+    }
 
-  private List<Product> buildAllProductsMock() {
-    return List.of(
-        Product.builder()
-            .id(1L)
-            .name("Space Helmet")
-            .description("A durable helmet for intergalactic travel.")
-            .price(BigDecimal.valueOf(299.99))
-            .stockQuantity(50)
-            .category(Category.builder().id(1L).name("Space Gear").build())
-            .build(),
-        Product.builder()
-            .id(2L)
-            .name("Anti-Gravity Boots")
-            .description("Experience weightlessness on any surface.")
-            .price(BigDecimal.valueOf(199.99))
-            .stockQuantity(30)
-            .category(Category.builder().id(2L).name("Space Wear").build())
-            .build(),
-        Product.builder()
-            .id(3L)
-            .name("Star Map")
-            .description("A holographic map of the known universe.")
-            .price(BigDecimal.valueOf(149.99))
-            .stockQuantity(100)
-            .category(Category.builder().id(3L).name("Space Tools").build())
-            .build()
-    );
-  }
+    private boolean existByName(String productName) {
+        return productRepository.getAll().stream()
+                .anyMatch(product -> product.getName().equals(productName));
+    }
+
+    private boolean existById(Long productId) {
+        return productRepository.getAll().stream()
+                .anyMatch(product -> product.getId().equals(productId));
+    }
+
+    private long setId() {
+        long id = productRepository.getAll().size() + 1;
+        return existById(id) ? id + 1 : id;
+    }
+
+    private Product buildProduct(Product product, Long categoryId, Long id) {
+        if (existByName(product.getName())) {
+            throw new DuplicateProductNameException(product.getName());
+        }
+        return product.toBuilder()
+                .category(Category.builder().id(categoryId).name("Space item").build())
+                .id(id)
+                .build();
+    }
+
 }
