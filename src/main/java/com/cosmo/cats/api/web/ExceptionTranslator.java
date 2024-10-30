7d12bc2cd00505be0fc186fa2cefbeb745eac3fe
Dima Artamonov <artamonovdima355@gmail.com>
2024-10-31 00:26:35 +0200

    Add exception handling

--- src/main/java/com/cosmo/cats/api/web/ExceptionTranslator.java
+++ src/main/java/com/cosmo/cats/api/web/ExceptionTranslator.java
@@ -1,5 +1,32 @@
 package com.cosmo.cats.api.web;
 
-public class ExceptionTranslator {
+import com.cosmo.cats.api.service.exception.DuplicateProductNameException;
+import com.cosmo.cats.api.service.exception.ProductNotFoundException;
+import java.net.URI;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionTranslator extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ProductNotFoundException.class)
+    ProblemDetail handleProductNotFoundException(ProductNotFoundException ex) {
+        ProblemDetail problemDetail =
+                ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setType(URI.create("product-not-found"));
+        problemDetail.setTitle("Product not found");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(DuplicateProductNameException.class)
+    ProblemDetail handleDuplicateProductNameException(DuplicateProductNameException ex) {
+        ProblemDetail problemDetail =
+                ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+        problemDetail.setType(URI.create("this-name-exists"));
+        problemDetail.setTitle("Duplicate name");
+        return problemDetail;
+    }
 
 }
